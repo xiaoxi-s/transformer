07fb7bcbfa835885a854a7ceab841c5c63d4a584
xiaoxi <77871407@qq.com>
2024-01-06 23:42:38 -0600

    fix encoder + pre_encoder -> decoder issue

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ from models.transformer import Transformer
 
 if __name__ == "__main__":
     print("Hello World!")
+    block_size = 8
+    dmodel = 256 
 
-    model = Transformer(10000, num_of_decoder_layers=1, num_of_encoder_layers=1) 
+    model = Transformer(10000, block_size=block_size, dmodel=dmodel) 
 
-    print(model)
-
-    print(model(torch.rand(1, 10, 512), torch.rand(1, 10, 512)).shape)
+    # print(model)
+    x = torch.randint(0, 10000, (1, 10), dtype=torch.int64)
+    y = torch.randint(0, 10000, (1, 10), dtype=torch.int64)
+    print(model(x, y).shape)
--- models/transformer.py
+++ models/transformer.py
@@ -4,9 +4,13 @@ import torch.nn as nn
 from .attention import AttentionLayer 
 
 class Transformer(nn.Module):
-    def __init__(self, vocab_size, dmodel=512, num_of_encoder_layers=6, num_of_decoder_layers=6, num_of_heads=8):
+    def __init__(self, vocab_size, block_size, dmodel=512, num_of_encoder_layers=6, num_of_decoder_layers=6, num_of_heads=8):
         super(Transformer, self).__init__()
         self.dmodel = dmodel 
+        self.block_size = block_size
+
+        self.embeddings = nn.Embedding(vocab_size, self.dmodel)
+        self.position_embedding_table = nn.Embedding(block_size, self.dmodel)
 
         self.encoder = nn.ModuleDict({
             f"encoder_layer_{i}": AttentionLayer(self.dmodel, num_of_heads, False)
@@ -31,9 +35,18 @@ class Transformer(nn.Module):
     def forward(self, x, y):
         # x: embeddings of input
         # y: embeddings of previous output
+        x = x[:, -self.block_size:]
+        y = y[:, -self.block_size:]
 
-        encodings = torch.cat([self.encoder[f"encoder_layer_{i}"](x, x, x) for i in range(len(self.encoder))], dim=-1)
-        pre_decodings = torch.cat([self.pre_decoder[f"pre_decoder_layer_{i}"](y, y, y) for i in range(len(self.pre_decoder))], dim=-1)
+        x = self.embeddings(x) + self.position_embedding_table(torch.arange(x.shape[1]))
+        y = self.embeddings(y) + self.position_embedding_table(torch.arange(y.shape[1]))
+
+        # encodings = torch.cat([self.encoder[f"encoder_layer_{i}"](x, x, x) for i in range(len(self.encoder))], dim=-1)
+        # pre_decodings = torch.cat([self.pre_decoder[f"pre_decoder_layer_{i}"](y, y, y) for i in range(len(self.pre_decoder))], dim=-1)
+        encodings = [self.encoder[f"encoder_layer_{i}"](x, x, x) for i in range(len(self.encoder))]
+        pre_decodings = [self.pre_decoder[f"pre_decoder_layer_{i}"](y, y, y) for i in range(len(self.pre_decoder))]
+
+        print("shapes: ", len(pre_decodings), len(encodings))
         output = torch.cat([self.decoder[f"decoder_layer_{i}"](pre_decodings[i], encodings[i], encodings[i]) for i in range(len(self.decoder))], dim=-1)
 
         output = self.fully_connected(output)
