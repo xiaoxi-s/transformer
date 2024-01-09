0b6873cf737368af2de969b62b2583841577ca97
xiaoxi <77871407@qq.com>
2024-01-08 23:34:54 -0600

    cuda:0

--- hyperparams.py
+++ hyperparams.py
@@ -5,4 +5,4 @@ batch_size = 32
 dmodel = 256 
 learning_rate = 0.00001
 cuda_available = torch.cuda.is_available()
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
