4600d0f2f06acc41125f2835b58694ba1b34b4d8
xiaoxi <77871407@qq.com>
2024-01-08 21:48:13 -0600

    args for eval.py

--- eval.py
+++ eval.py
@@ -1,3 +1,5 @@
+import argparse
+
 import torch
 import torch.nn as nn
 
@@ -5,7 +7,12 @@ from data.utils import load_pickled_data, get_train_and_test_dataset, generate_c
 from models.transformer import Transformer
 from hyperparams import *
 
+
 if __name__ == "__main__":
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('-e', '--epoch', type=int, default=150)
+    parser = argparser.parse_args()
+    epoch = parser.epoch
     print("Hello World!")
     vocab_to_ind = load_pickled_data('vocab_to_ind.pkl') 
     ind_to_vocab = load_pickled_data('ind_to_vocab.pkl')
@@ -13,7 +20,7 @@ if __name__ == "__main__":
     print("Token type number: ", len(vocab_to_ind))
 
     model = Transformer(len(vocab_to_ind), dropout=0.2, block_size=block_size, num_of_decoder_layers=2, num_of_encoder_layers=2, dmodel=dmodel).to(device) 
-    model.load_state_dict(torch.load('data/model-200.pth'))
+    model.load_state_dict(torch.load(f'data/model-{epoch}.pth'))
     model.eval()
 
     generate_contents(model, vocab_to_ind, ind_to_vocab=ind_to_vocab, device=device, max_num_of_tokens=1000)
