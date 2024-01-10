74e58da70cec1e0b59fdd53eb22aa43128e79fed
xiaoxi-s <77871407@qq.com>
2024-01-10 02:10:12 +0000

    fix model

--- eval.py
+++ eval.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
 
     model = Transformer(len(vocab_to_ind), dropout=dropout, block_size=block_size, num_of_decoder_layers=1, num_of_encoder_layers=1, dmodel=dmodel).to(device) 
     if parser.parallel.lower() == "true" or parser.parallel.lower() == "t":
-        model = nn.DataParallel(Transformer(len(vocab_to_ind), dropout=dropout, block_size=block_size, num_of_decoder_layers=2, num_of_encoder_layers=2, dmodel=dmodel).to(device)) 
+        model = nn.DataParallel(Transformer(len(vocab_to_ind), dropout=dropout, block_size=block_size, num_of_decoder_layers=1, num_of_encoder_layers=1, dmodel=dmodel).to(device)) 
     
     model.load_state_dict(torch.load(f'data/model-{epoch}.pth'))
     model.eval()
