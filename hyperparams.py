e347a4ed150938d6bc6643c2b579a35e33fc75e5
xiaoxi <77871407@qq.com>
2024-08-19 11:14:03 -0500

    not restricting to cuda 0 device

--- hyperparams.py
+++ hyperparams.py
@@ -6,4 +6,4 @@ dmodel = 256
 learning_rate = 0.00001
 cuda_available = torch.cuda.is_available()
 dropout = 0.4
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
