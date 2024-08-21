fe2356ac4c33f603e25c48f451aab0fdb5da5aed
xiaoxi <77871407@qq.com>
2024-08-21 00:42:49 -0500

    refactor train.py to its file

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import argparse
-
+import wandb
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -12,90 +12,13 @@ from matplotlib import pyplot as plt
 from models.transformer import Transformer 
 from data.utils import load_pickled_data, get_train_and_test_dataset
 from hyperparams import *
-
-import wandb
-
-def train(model, train_loader, test_loader, criterion, optimizer, epochs=1):
-    # Define loss function and optimizer
-    # Training loop
-    train_loss_history = []
-    test_loss_history = []
-    epoch_sequence = []
-
-    plt.xlabel('Epoch')
-    plt.ylabel('Loss')
-    plt.title('Loss History')
-
-    for epoch in range(epochs):
-        # training
-        log_registry = {}
-        training_loss = 0.0
-        with tqdm(total=len(train_loader), desc=f'Epoch {epoch + 1}/{epochs}', unit='batch') as pbar:
-            model.train()
-            for batch in train_loader:
-                inputs, labels = batch[:, 0, :].contiguous(), batch[:, 1, :].contiguous()
-                inputs, labels = inputs.to(device), labels.to(device)
-                optimizer.zero_grad()  # Zero the gradients
-                logits = model(inputs, inputs)  # Forward pass: (B, T, Emb)
-                B, T, C = logits.shape
-                logits = logits.view(B * T, C)
-                labels = labels.view(B * T)
-
-                loss = criterion(logits, labels)  # Compute the loss
-                loss.backward()  # Backpropagation
-                optimizer.step()  # Update weights
-
-                training_loss += (loss.item() * len(batch))
-                pbar.update(1)  # Update the progress bar
-        train_loss_history.append(training_loss / len(train_loader.dataset))
-        log_registry['train_loss'] = training_loss / len(train_loader.dataset)
-
-        # testing
-        test_loss = 0.0    
-        with torch.no_grad():
-            model.eval()
-            for batch in test_loader:
-                inputs, labels = batch[:, 0, :].contiguous(), batch[:, 1, :].contiguous()
-                inputs, labels = inputs.to(device), labels.to(device)
-                logits = model(inputs, inputs)  # Forward pass: (B, T, Emb)
-                B, T, C = logits.shape
-                logits = logits.view(B * T, C)
-                labels = labels.view(B * T)
-
-                loss = criterion(logits, labels)  # Compute the loss
-                test_loss += (loss.item() * len(batch))
-        test_loss_history.append(test_loss / len(test_loader.dataset))
-        log_registry['test_loss'] = test_loss / len(test_loader.dataset)
-        wandb.log(log_registry)
-
-        epoch_sequence.append(epoch + 1)
-
-        # plt.plot(epoch_sequence, train_loss_history, 'b-', label='train loss')
-        # plt.plot(epoch_sequence, test_loss_history, 'r-', label='test loss')
-        # plt.show(block=False)
-
-        # if epoch == 0:
-        #     plt.legend()
-
-        # plt.pause(0.001)
-
-        if epoch > 1:
-            print(f'Epoch {epoch + 1}/{epochs}: train loss {train_loss_history[-1]}, test loss {test_loss_history[-1]}')
-
-        if epoch > 1 and epoch % 3 == 0:
-            # Save the model
-            torch.save(model.state_dict(), f'./data/model-{epoch}.pth')
-
-    print('Training complete!')
-    
-    return model 
+from train import train
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-                    prog='ProgramName',
-                    description='What the program does',
-                    epilog='Text at the bottom of help')
+                    prog='shakespear-training',
+                    description='pretrain shakespeare transformer')
     parser.add_argument('-e', '--epochs', default=20, type=int)           # positional argument
     parser.add_argument('-f', '--factor', default=0.0001, type=float)      # option that takes a value
     parser.add_argument('-p', '--parallel', default="true", type=str)      # option that takes a value
---
+++ train.py
@@ -0,0 +1,69 @@
+
+import torch
+import wandb
+
+from tqdm import tqdm
+from hyperparams import *
+
+
+def train(model, train_loader, test_loader, criterion, optimizer, epochs=1):
+    # Define loss function and optimizer
+    # Training loop
+    train_loss_history = []
+    test_loss_history = []
+    epoch_sequence = []
+
+    for epoch in range(epochs):
+        # training
+        log_registry = {}
+        training_loss = 0.0
+        with tqdm(total=len(train_loader), desc=f'Epoch {epoch + 1}/{epochs}', unit='batch') as pbar:
+            model.train()
+            for batch in train_loader:
+                inputs, labels = batch[:, 0, :].contiguous(), batch[:, 1, :].contiguous()
+                inputs, labels = inputs.to(device), labels.to(device)
+                optimizer.zero_grad()  # Zero the gradients
+                logits = model(inputs, inputs)  # Forward pass: (B, T, Emb)
+                B, T, C = logits.shape
+                logits = logits.view(B * T, C)
+                labels = labels.view(B * T)
+
+                loss = criterion(logits, labels)  # Compute the loss
+                loss.backward()  # Backpropagation
+                optimizer.step()  # Update weights
+
+                training_loss += (loss.item() * len(batch))
+                pbar.update(1)  # Update the progress bar
+        train_loss_history.append(training_loss / len(train_loader.dataset))
+        log_registry['train_loss'] = training_loss / len(train_loader.dataset)
+
+        # testing
+        test_loss = 0.0    
+        with torch.no_grad():
+            model.eval()
+            for batch in test_loader:
+                inputs, labels = batch[:, 0, :].contiguous(), batch[:, 1, :].contiguous()
+                inputs, labels = inputs.to(device), labels.to(device)
+                logits = model(inputs, inputs)  # Forward pass: (B, T, Emb)
+                B, T, C = logits.shape
+                logits = logits.view(B * T, C)
+                labels = labels.view(B * T)
+
+                loss = criterion(logits, labels)  # Compute the loss
+                test_loss += (loss.item() * len(batch))
+        test_loss_history.append(test_loss / len(test_loader.dataset))
+        log_registry['test_loss'] = test_loss / len(test_loader.dataset)
+        wandb.log(log_registry)
+
+        epoch_sequence.append(epoch + 1)
+
+        if epoch > 1:
+            print(f'Epoch {epoch + 1}/{epochs}: train loss {train_loss_history[-1]}, test loss {test_loss_history[-1]}')
+
+        if epoch > 1 and epoch % 3 == 0:
+            # Save the model
+            torch.save(model.state_dict(), f'./data/model-{epoch}.pth')
+
+    print('Training complete!')
+    
+    return model 
