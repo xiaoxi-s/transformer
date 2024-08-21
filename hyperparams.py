46142e5f88f743579cf5f86584b73da7801bd909
xiaoxi <77871407@qq.com>
2024-08-21 03:18:16 -0500

    set dropout to be 0.2

--- hyperparams.py
+++ hyperparams.py
@@ -5,7 +5,7 @@ batch_size = 128
 dmodel = 256 
 learning_rate = 0.00001
 cuda_available = torch.cuda.is_available()
-dropout = 0.4
+dropout = 0.2
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 finetune_learning_rate = 1e-6
