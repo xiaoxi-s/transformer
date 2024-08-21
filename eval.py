7ee271186331089e14486db13cd60cd3932cd3b0
xiaoxi <77871407@qq.com>
2024-08-21 00:08:14 -0500

    max token param for eval.py

--- eval.py
+++ eval.py
@@ -14,22 +14,25 @@ if __name__ == "__main__":
     np.random.seed(7777)
     argparser = argparse.ArgumentParser()
     argparser.add_argument('-e', '--epoch', type=int, default=150)
+    argparser.add_argument('-m', '--max-token', type=int, default=1000)
     argparser.add_argument('-p', '--parallel', default="true", type=str)      # option that takes a value
 
     parser = argparser.parse_args()
     epoch = parser.epoch
-    print("Hello World!")
+    max_token = parser.max_token
+    print("Loading vocab ...")
     vocab_to_ind = load_pickled_data('vocab_to_ind.pkl') 
     ind_to_vocab = load_pickled_data('ind_to_vocab.pkl')
+    print("Done loading vocab ...")
     torch.set_default_device(device)
-    print("Token type number: ", len(vocab_to_ind))
+    print("Vocab size: ", len(vocab_to_ind))
 
     model = Transformer(len(vocab_to_ind), dropout=dropout, block_size=block_size, num_of_decoder_layers=1, num_of_encoder_layers=1, dmodel=dmodel).to(device) 
     if parser.parallel.lower() == "true" or parser.parallel.lower() == "t":
         model = nn.DataParallel(Transformer(len(vocab_to_ind), dropout=dropout, block_size=block_size, num_of_decoder_layers=1, num_of_encoder_layers=1, dmodel=dmodel).to(device)) 
-    
+
     model.load_state_dict(torch.load(f'data/model-{epoch}.pth'))
     model.eval()
 
-    generate_contents(model, vocab_to_ind, ind_to_vocab=ind_to_vocab, device=device, max_num_of_tokens=1000)
+    generate_contents(model, vocab_to_ind, ind_to_vocab=ind_to_vocab, device=device, max_num_of_tokens=max_token)
     
