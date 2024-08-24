bd86ef01e53fb7b4161f338ae127dc2ae07eed55
xiaoxi <77871407@qq.com>
2024-08-24 03:02:58 -0500

    better hyperparam printing

--- hyperparams.py
+++ hyperparams.py
@@ -7,7 +7,10 @@ learning_rate = 0.00001
 cuda_available = torch.cuda.is_available()
 dropout = 0.2
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+num_of_decoder_layers=1
+num_of_encoder_layers=1
 
-finetune_learning_rate = 1e-6
-finetune_block_size = 3
-finetune_batch_size = 16
+# Not used for now
+# finetune_learning_rate = 1e-6
+# finetune_block_size = 3
+# finetune_batch_size = 16
